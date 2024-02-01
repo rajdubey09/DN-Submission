c2e0ada12127ddf2b300047143bfba04215873f6
Raj Dubey <55090463+rajdubey09@users.noreply.github.com>
2024-02-01 20:32:51 +0530

    Add files via upload
    initial commit

---
+++ flaskApp/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+from dataneuron_submission import *
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def form():
+    if request.method == "GET":
+        return render_template('form.html')
+
+    else:
+        sentence1 = str(request.form['sentence1'])
+        sentence2 = str(request.form['sentence2'])
+        sim_res = siml(sentence1,sentence2)
+
+        return render_template('form.html', sim_score=int(sim_res))
